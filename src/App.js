fd87c89948489ce7c3e5d0cd8ea691e6145cf782
Mayank <w1mayankz@gmail.com>
2026-08-01 10:29:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -495,7 +495,7 @@ export default function App() {
               </div>
             </div>
 
-            <div className="mt-24 w-full max-w-3xl mx-auto flex flex-col gap-4 px-4 md:px-0">
+            <div className="mt-24 w-full max-w-3xl mx-auto flex flex-col gap-4 md:px-0">
                 <img 
                   src="/wxotwin.jpg"
                   alt="testimonial image"
